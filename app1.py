8a08dab858d6e36621eb52998816b7e0a99cf528
omkar <wareomkar12355@gmail.com>
2025-03-22 15:32:40 +0530

    added sub function in  feature branch

--- app1.py
+++ app1.py
@@ -1,11 +1,12 @@
 import sys
-def add(num1,num2):
-        num3=num1+num2
+def sub(num1,num2):
+        num3=num1-num2
         print(num3)
 num1=int(sys.argv[1])
 operator=sys.argv[2]
 num2=int(sys.argv[3])
-if operator == "add":
-    add(num1,num2)
+if operator == "sub":
+    sub(num1,num2)
 else :
       print("error")
+

610fffbd95ddcb1bf968f3df4da83b6f6bd3a3f8
omkar <wareomkar12355@gmail.com>
2025-03-22 15:27:50 +0530

    added addition function in main branch:

--- app1.py
+++ app1.py
@@ -1,1 +1,11 @@
-print("hello-world")
+import sys
+def add(num1,num2):
+        num3=num1+num2
+        print(num3)
+num1=int(sys.argv[1])
+operator=sys.argv[2]
+num2=int(sys.argv[3])
+if operator == "add":
+    add(num1,num2)
+else :
+      print("error")

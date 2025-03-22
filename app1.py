b04f2fb86a7f129f2d863081b06a64144b03aa6e
omkar <wareomkar12355@gmail.com>
2025-03-22 15:46:49 +0530

    add

--- app1.py
+++ app1.py
@@ -1,11 +1,11 @@
 import sys
-def sub(num1,num2):
-        num3=num1-num2
+def add(num1,num2):
+        num3=num1+num2
         print(num3)
 num1=int(sys.argv[1])
 operator=sys.argv[2]
 num2=int(sys.argv[3])
-if operator == "sub":
+if operator == "add":
     sub(num1,num2)
 else :
       print("error")
